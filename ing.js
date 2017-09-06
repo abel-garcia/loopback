2089aa4679a41c3f509625308eb941e2e0c3c1d7
Abel Garcia <agarcia@gabinete.mx>
2017-09-05 19:53:59 -0500

    create users

---
+++ ing.js
@@ -0,0 +1,27 @@
+/*
+model users :
+  name: {
+    fistName:
+    lastName:
+  },
+  login: {
+    email:
+    password:
+  },
+  picture:{
+    large:
+    medium:
+    normal:
+  },
+  person:{
+    description:
+    location:{
+      street:
+      city :
+      latitud:
+      longitud:
+    }
+  }
+  updateat:
+  createat:
+*/
