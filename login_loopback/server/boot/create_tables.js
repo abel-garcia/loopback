5fc9ad9a4a1b1238be49b1e0048fb43dc099428f
Abel Garcia <agarcia@gabinete.mx>
2017-09-11 19:26:28 -0500

    login

---
+++ login_loopback/server/boot/create_tables.js
@@ -0,0 +1,23 @@
+module.exports = function(server) {
+  // server.dataSources.mysql.automigrate('ACL', function(err) {
+  //   if (err) throw err;
+  //   console.log("Create ACL");
+  // });
+  // server.dataSources.mysql.automigrate('User', function(err) {
+  //   if (err) throw err;
+  //   console.log("Create User");
+  // });
+  // server.dataSources.mysql.automigrate('AccessToken', function(err) {
+  //   if (err) throw err;
+  //   console.log("Create AccessToken");
+  // });
+  // server.dataSources.mysql.automigrate('RoleMapping', function(err) {
+  //   if (err) throw err;
+  //   console.log("Create RoleMapping");
+  // });
+  // server.dataSources.mysql.automigrate('Role', function(err) {
+  //   if (err) throw err;
+  //   console.log("Create Role");
+  // });
+  //Chat
+};
